8d676ba86e546d7d5fe6d3d9efc4d51e220311cf
Yugandhar Deshmukh <yugsjdeshmukh194@gmail.com>
2025-02-13 10:31:08 +0530

    First commit

---
+++ backend/tickethub/src/main/java/com/tickethub/dto/BookingRequestDTO.java
@@ -0,0 +1,5 @@
+package com.tickethub.dto;
+
+public @interface BookingRequestDTO {
+
+}
---
+++ backend/tickethub/src/main/java/com/tickethub/service/EmailService.java
@@ -0,0 +1,16 @@
+package com.tickethub.service;
+
+import com.tickethub.entities.EmailDetails;
+
+public interface EmailService {
+
+	// Method
+	// To send a simple email
+	String sendSimpleMail(EmailDetails details);
+
+	// Method
+	// To send an email with attachment
+	String sendMailWithAttachment(EmailDetails details);
+
+	String sendMailWithAttachment(EmailDetails emailDetails, byte[] pdfBytes);
+}
