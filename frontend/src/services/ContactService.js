4d2ee55983095c3ab97f479bcff3fd22c213dd9c
Yugandhar Deshmukh <102009488+Yugandhar194@users.noreply.github.com>
2025-02-21 23:47:19 +0530

    Delete frontend directory

--- frontend/src/services/ContactService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { CONTACT_BASE_URL } from "../constants/ApiConstants";
-
-export function sendMessage(data) {
-    return axios.post(`${CONTACT_BASE_URL}send`,data);
-}
--- frontend/src/services/TicketService.js
+++
@@ -1,7 +0,0 @@
-import { TICKET_BASE_URL } from "../constants/ApiConstants";
-
-export function getTicket(bookingId) { 
-    return axios.get(`${TICKET_BASE_URL}/download/${bookingId}`, {
-        responseType: "blob", // Important for handling PDF
-      });
-}
